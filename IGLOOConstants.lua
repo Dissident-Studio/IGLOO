0c48767d5fd48273c976b0645b2ac4f4d7c65cca
Kranck <frizzy.rastay@gmail.com>
2022-10-26 17:06:36 +0200

    Missing header comment in IGLOO constants

--- IGLOOConstants.lua
+++ IGLOOConstants.lua
@@ -1,6 +1,16 @@
---
--- Contain som constants for IGLOO
---
+--[[
+	 ____  _         _   _         _      _____ _         _ _
+	|    \|_|___ ___|_|_| |___ ___| |_   |   __| |_ _ _ _| |_|___
+	|  |  | |_ -|_ -| | . | -_|   |  _|  |__   |  _| | | . | | . |
+	|____/|_|___|___|_|___|___|_|_|_|    |_____|_| |___|___|_|___|
+
+	This project is under the GNU GPLv3 license. Please refer to the LICENSE file for more informations
+
+	Project			:	IGLOO
+	Author			:	Yannis Beaux (Kranck) [yannis.beaux@dissidentstudio.fr]
+	Date			:	26 / 10/ 2022
+	Description		:	Contains some constants used in the project (temporary)
+--]]
 -- Note : Temporary for now, in the end it should be some kind of singleton
 
 G_IGLOOConfigFilename = "igloo-config.lua"
